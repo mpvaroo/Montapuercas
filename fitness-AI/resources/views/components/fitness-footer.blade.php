184737a17b1d2d841d0948d20f72724888659960
Manuel <mvarper@iesmarquesdecomares.org>
2026-01-26 11:01:10 +0100

    siu

---
+++ fitness-AI/resources/views/components/fitness-footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container footer-container">
+        <div class="copy">© 2026 AutomAI Gym. Design by BOLD.</div>
+
+        <div class="socials">
+            <a href="#">Instagram</a>
+            <a href="#">Twitter</a>
+            <a href="#">Support</a>
+        </div>
+    </div>
+</footer>
--- fitness-AI/routes/web.php
+++ fitness-AI/routes/web.php
@@ -6,8 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::view('/usuarios', 'usuarios');
+Route::get('/fitness-home', function () {
+    return view('fitness-home');
+});
+
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
