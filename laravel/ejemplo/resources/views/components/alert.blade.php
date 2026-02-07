2e9b700fa78b0f689d9e06313420ff77ffde16ae
manuelmvp2004 <manuelmvp2004@gmail.com>
2026-02-07 16:58:02 +0100

    feat: Add project views and layouts archive.

--- laravel/ejemplo/resources/views/components/alert.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="p-4 mb-4 text-sm text-{{ $colortext }}-800 rounded-lg bg-{{ $colorbg }}-50 dark:bg-{{ $colorbg }}-800 dark:text-{{ $colortext }}-400"
-    role="alert">
-    <span>{{ $slot }}</span><br>
-    {{ $peligro() }}
-</div>
-</br>
-<p {{ $attributes }}>POOOOOOOOO</p>
-</br>
--- laravel/laravel_livewire_auth/resources/views/productos.blade.php
+++
@@ -1,2 +0,0 @@
-<x-layouts::app :title="__('Dashboard')">    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">     Lista de
-        Productos    </div></x-layouts::app>
